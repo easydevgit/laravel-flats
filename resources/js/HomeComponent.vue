837639729ac7fe02134606ce8f64e3603c4b5085
easydevgit <urserg1@mail.com>
2023-04-06 17:10:59 +0400

    feat(vue): vue install

---
+++ resources/js/HomeComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <div class="card">
+          <div class="card-header">Example Component</div>
+
+          <div class="card-body">
+            I'm an Home component. {{name}}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import {ref} from "vue";
+
+  const name = ref<string>('Home');
+</script>
